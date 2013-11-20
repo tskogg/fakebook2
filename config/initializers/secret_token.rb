16b53c75e5985b940945e3332c1aac0f98197654
Mike Gore <gore@colorado.edu>
2013-11-19 23:21:07 -0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fakebook::Application.config.secret_token = 'b99a962cff8029a33ddd61abf14bf59ac17e9e14084e21ee14f5c03fdc5f6ca58004ef444a1bf37feb3133dc1a2277384ccf618c741e587a1995492f43b81347'
